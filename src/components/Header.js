a74db148c82c2336abc48e0d2a2e8dcd0fbadfb1
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-04-24 20:46:45 -0300

    finishing mainScreen

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,12 +14,12 @@ export default class Header extends Component {
             <h1>Unlimited movies, TV shows, and more.</h1>
             <h2>Watch anywhere. Cancel anytime.</h2>
             <h3>Ready to watch? Enter your email to create or access your account.</h3>
-            <form>
+            <form className="inline-form">
               <input 
               type="text" 
               placeholder="Email adress" 
               name="email"/>
-              <button>JOIN NOW ></button>
+              <button className="btn">JOIN NOW ></button>
             </form>
           </div>
         </header>
