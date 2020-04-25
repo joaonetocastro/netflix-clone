7fda9625b44f81e46b959f4fedc78028ab89507e
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-04-25 10:25:13 -0300

    Login Screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,13 @@ export default class Header extends Component {
             <NetflixLogo/>
             <Button to='/login'>Sign In</Button>
           </div>
-          <div className="header-content">
+          <div className="header-content inline-form">
             <h1>Unlimited movies, TV shows, and more.</h1>
             <h2>Watch anywhere. Cancel anytime.</h2>
             <h3>Ready to watch? Enter your email to create or access your account.</h3>
-            <form className="inline-form">
+            <form>
               <input 
-              type="text" 
+              type="email" 
               placeholder="Email adress" 
               name="email"/>
               <Button>JOIN NOW ></Button>
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
-
+import NetflixLogo from '../components/NetflixLogo';
+import Button from '../components/Button';
 // import { Container } from './styles';
 
 export default class LoginScreen extends Component {
   render() {
-    return <div />;
+    return (
+      <header className="header">
+          <div className="header-top">
+            <NetflixLogo/>
+          </div>
+          <div className="header-content">
+            <div className="header-dark">
+              <h1>Sign In</h1>
+              <form>
+                <input 
+                  type="email" 
+                  placeholder="Email or phone number" 
+                  name="email"/>
+                <input 
+                  type="password" 
+                  placeholder="Password" 
+                  name="password"/>
+                <Button to='/browse'>Sign In</Button>
+              </form>
+            </div>
+          </div>
+        </header>
+    );
   }
 }
